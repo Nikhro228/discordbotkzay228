36753da76ea85a8d252338661c433eb9eeddbc5b
Nikhro228 <78687450+Nikhro228@users.noreply.github.com>
2021-02-21 14:00:34 +0300

    DISCORD
    D

---
+++ Procfile.txt
@@ -0,0 +1,1 @@
+worker: node kzay.js
---
+++ kzay.js
@@ -0,0 +1,239 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау Кчау') {
+    msg.reply('Кчау!');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '!кчау') {
+    msg.reply('такой команды не существует,напишите: !кчау хелп чтоюы узнать все команды');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '!кчау а') {
+    msg.reply('такой команды не существует,напишите: !кчау хелп чтоюы узнать все команды');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау б') {
+    msg.reply('такой команды не существует,напишите: !кчау хелп чтоюы узнать все команды');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау хелп') {
+    msg.reply('https://sites.google.com/view/commandsbotkzay');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау Мать жива?') {
+    msg.reply('да а у тебя?');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау да') {
+    msg.reply('В смысле? Я же её в канаве закопал');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау Нет') {
+    msg.reply('хахахахахахах,безмамный');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка1') {
+    msg.reply('мы твою мать,толпой выебали');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка2') {
+    msg.reply('твоя мать сегодня хорошо сосала');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка3') {
+    msg.reply('вчера устроили заезд с друзьями,пустили твою мать по кругу');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка4') {
+    msg.reply('мы твою мать,толпой выебали');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка5') {
+    msg.reply('мы твою мать в космос отправили');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка6') {
+    msg.reply('у тебя мать в канаве');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка7') {
+    msg.reply('пёрнул в ебло твоей матери');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка8') {
+    msg.reply('я твою маму на авито продал');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка9') {
+    msg.reply('я твою мать на ставках проебал');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка10') {
+    msg.reply('мы твою мать на запчасти разобрали');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка11') {
+    msg.reply('я твою мать загрыз нахуй');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка12') {
+    msg.reply('я твою мать нахуй переехал');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка13') {
+    msg.reply('я твою мать в мусорку выкинул');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка14') {
+    msg.reply('проткнул твою мать в шею и тут же выебал в эту дырку');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка15') {
+    msg.reply('я твою мать на глушитель сажал и на поршнях вертел');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка16') {
+    msg.reply('у вас мать сгорела');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка17') {
+    msg.reply('вчера видел твою мать голой в мусорке');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка18') {
+    msg.reply('воткнул в комп твою мать, она сгорела нахуй');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка19') {
+    msg.reply('приехал забрать твою мать в сосательные войска');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка20') {
+    msg.reply('я твою маму в майнкрафте загриферил и выебал');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка21') {
+    msg.reply('на войне нужна была шлюха,которая отсосёт всей армии твоя мать подошла больше всего');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка22') {
+    msg.reply('твоя мамка болше киберпанка весит');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка23') {
+    msg.reply('я твою мать на юле продал');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка24') {
+    msg.reply('насадил твою мамашу на армейский пулемёт');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка25') {
+    msg.reply('я твою мать в туалет сымл');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка26') {
+    msg.reply('ебал твою мать,пока ты был в школе');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка27') {
+    msg.reply('пока медведи сосут лапу,твоя мать сосёт хуй');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка28') {
+    msg.reply('твоя мать отлично подходит для озвучки хентая');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка29') {
+    msg.reply('я твою мать на металлолом сдал');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '!кчау шутка30') {
+    msg.reply('вижу твою мать за километр,пиздец она жирная');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
